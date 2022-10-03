676a6f911050ecc9b4b741046e188a90327c703e
toxidxd <toxid13@yandex.ru>
2022-10-03 23:14:43 +0300

    some fixes

--- task_1.py
+++ task_1.py
@@ -14,3 +14,5 @@ print('Задача 1. Тайны археологии')
 for number in 114, 12, 14, 10605, 4907, 450:
     if number % 2 == 0 and number % 3 != 0:
         print(f"Число {number} подходит")
+    else:
+        print(f"Число {number} не подходит")
